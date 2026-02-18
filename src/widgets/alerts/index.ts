1519400905a903244bae23062458fd92f14e5dd7
Teran Sarathchandra <teran8777@gmail.com>
2026-02-18 22:57:08 +0530

    refactor(ui): remove unreferenced pipeline and alert modules

--- src/widgets/alerts/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AlertsSlot } from '@/widgets/alerts/AlertsSlot'
-export { useAlerts } from '@/widgets/alerts/useAlerts'
-export type { AlertData, AlertsComponentProps } from '@/widgets/alerts/types'
